5be4a3036f20d2065229ecc2c298eac620408c78
Minjune Hwang <mjhwang@berkeley.edu>
2024-04-29 18:08:07 +0900

    fix logdir in reach example

--- example_reach.py
+++ example_reach.py
@@ -9,7 +9,7 @@ def main():
   config = embodied.Config(dreamerv3.configs['defaults'])
   config = config.update(dreamerv3.configs['medium'])
   config = config.update({
-      'logdir': '~/logdir/run1',
+      'logdir': '~/logdir/fetchreach1',
       'run.train_ratio': 64,
       'run.log_every': 30,  # Seconds
       'batch_size': 16,
