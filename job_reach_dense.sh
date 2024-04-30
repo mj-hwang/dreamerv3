514686b78b812e5f845a0f156caf101a3bfdd6ea
Minjune Hwang <mjhwang@berkeley.edu>
2024-04-30 14:25:02 +0900

    add emails to job scripts

--- job_reach_dense.sh
+++ job_reach_dense.sh
@@ -4,8 +4,11 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=60G
 #SBATCH --gres=gpu:2
+#SBATCH --mail-user=mjhwang@berkeley.edu
 
 export MUJOCO_GL='egl'
 conda activate mbgcrl
 module swap cuda/12.1
+
+cd dreamerv3
 python python example_reach.py
